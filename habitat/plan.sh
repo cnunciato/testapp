ae251d4f011b197bee3bb6a19878f671d900db2e
Christian Nunciato <chris@nunciato.org>
2018-03-28 16:53:28 -0700

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -5,8 +5,7 @@ pkg_maintainer="Christian Nunciato <chris@nunciato.org>"
 pkg_license=("MIT")
 
 do_install() {
-  build_line "SOME_VAL: ${SOME_VAL}"
-  echo $SOME_VAL
+  build_line "SOME_KEY: $SOME_KEY"
   return 0
 }
 
